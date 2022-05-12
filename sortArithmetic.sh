7813e12082ba0b031fa0133cf08760be491aa481
Inamul <inamulhoque097@gmail.com>
2022-05-12 10:57:27 +0530

    Modified: compute a*b+c

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -4,4 +4,6 @@ read -p "Enter the value of a : " a
 read -p "Enter the value of b : " b
 read -p "Enter the value of c : " c
 x=$(( $a + $b * $c ))
-echo $x
+y=$(( $a * $b + $c ))
+echo "$a+$b*$c = $x"
+echo "$a*$b+$c = $y"
