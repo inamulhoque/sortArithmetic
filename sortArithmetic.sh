04bd5d6320eb4c82175cfa09602e3602c4c62acf
Inamul <inamulhoque097@gmail.com>
2022-05-12 11:07:55 +0530

    modified: Compute a%b+c

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -3,10 +3,12 @@
 read -p "Enter the value of a : " a
 read -p "Enter the value of b : " b
 read -p "Enter the value of c : " c
+
 w=$(( $a + $b * $c ))
 x=$(( $a * $b + $c ))
 y=$((`expr $c + $a / $b | bc` ))
 z=$(( $a % $b + $c ))
+
 echo "$a+$b*$c = $w"
 echo "$a*$b+$c = $x"
 echo "$c+$a/$b = $y"
